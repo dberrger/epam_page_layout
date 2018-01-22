3d2949184752cb79bd7195a15dd67e9679d0fb0a
dberrger <andrew.glbv1997@gmail.com>
2018-01-22 15:44:48 +0300

    added new table rendering with js

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+
 let a = function () {
     let tableWrapper = document.getElementsByClassName("main-table-flex main-table")[0];
     console.dir(tableWrapper);
@@ -6,7 +7,7 @@ let a = function () {
     for (let i = 0; i < 5; i++) {
         let tr = document.createElement("tr");
         tr.className = "sb-content__calendar-row-color";
-
+        let flag = true;
         for(let j = 0; j < 7; j++) {
 
             let td = document.createElement("td");
@@ -15,33 +16,84 @@ let a = function () {
             let div_1 = document.createElement("div");
             div_1.className = "content";
 
+            if(counter === 3 && flag === true ) {
+                let div_2 = document.createElement("div");
+                div_2.className = "sb-content__calendar-note-1";
+                let div_2_inner = document.createElement("div");
+                div_2_inner.className = "sb-content__calendar-note-1-colorblock1";
+                let a = document.createElement("div");
+                a.className = "sb-content__calendar-todo-link tooltip";
+                a.innerHTML = "Letter #01-01 \"Whatever\" from Organization 1 to EPAM";
+
+                td.appendChild(div_2);
+                div_2.appendChild(div_2_inner);
+                div_2.appendChild(a);
+            }
+
+             for(let k = 1; k < 3; k++) {
+                 if ((counter === 7) && flag === true) {
+                     let div_2 = document.createElement("div");
+                     div_2.className = `sb-content__calendar-note-yellow-${k}`;
+                     let div_2_inner = document.createElement("div");
+                     div_2_inner.className = `sb-content__calendar-note-1-colorblock3`;
+                     let a = document.createElement("div");
+                     a.className = `sb-content__calendar-todo-link tooltip`;
+                     a.innerHTML = `Letter #01-01 \"Whatever\" from Organization 1 to EPAM`;
 
 
-            let div_2 = document.createElement("div");
-            div_2.className = "sb-content__calendar-note-1";
-            let div_2_inner = document.createElement("div");
-            div_2_inner.className = "sb-content__calendar-note-1-colorblock1";
-            let a = document.createElement("div");
-            a.className = "sb-content__calendar-todo-link tooltip";
+                     td.appendChild(div_2);
+                     div_2.appendChild(div_2_inner);
+                     div_2.appendChild(a);
+                 }
+             }
 
-            if(counter === 3 && counter === 7 && counter === 10 ) {
+            for(let k = 3; k < 7; k++) {
+                if((counter === 10) && flag === true ) {
+                    let div_2 = document.createElement("div");
+                    div_2.className = `sb-content__calendar-note-${k}`;
+                    let div_2_inner = document.createElement("div");
+                    div_2_inner.className = `sb-content__calendar-note-1-colorblock${k}`;
+                    let a = document.createElement("div");
+                    a.className = `sb-content__calendar-todo-link tooltip`;
+
+                    a.innerHTML = `Letter #01-01 \"Whatever\" from Organization 1 to EPAM`;
+
+                    td.appendChild(div_2);
+                    div_2.appendChild(div_2_inner);
+                    div_2.appendChild(a);
+                }
 
             }
+
 
             div_1.innerHTML = `${counter}`;
             if(counter === 32) {
                 div_1.innerHTML = `Sept 1`;
                 counter = 1;
+                flag = false;
             }
             td.appendChild(div_1);
-
             counter++;
 
         }
-
         tableWrapper.appendChild(tr);
 
     }
 };
 
 a();
+
+// window.onload = function(){
+//     var some = document.getElementsByClassName('tooltip')[0];
+//     some.style.color = '#fff';
+//     some.onmouseover = function() {
+//
+//         some.style.color = '#00ff3e';
+//         this.style.textDecoration='underline';
+//     };
+//     some.onmouseout = function(){
+//         some.style.color = '#f00';
+//         this.style.textDecoration='none';
+//     }
+//
+// }
