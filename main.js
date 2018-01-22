7702fa2788fa2091c5933c5aa6f255fb4dc7574c
dberrger <andrew.glbv1997@gmail.com>
2018-01-22 13:14:56 +0300

    Revert "Initial commit"
    added some js code

--- main.js
+++ main.js
@@ -1,17 +1,22 @@
-var a = function () {
-    let tableWrapper = document.getElementsByClassName("sb-content__calendar-rowwrapper")[0];
+let a = function () {
+    let tableWrapper = document.getElementsByClassName("main-table-flex main-table")[0];
     console.dir(tableWrapper);
-    for (let i = 0; i < 5; i++) {
-        let counter = 3;
+    let counter = 3;
 
-        let tr = document.createElement("tr").className = "sb-content__calendar-row-color";
+    for (let i = 0; i < 5; i++) {
+        let tr = document.createElement("tr");
+        tr.className = "sb-content__calendar-row-color";
 
         for(let j = 0; j < 7; j++) {
 
             let td = document.createElement("td");
             td.className = "main-td";
+            tr.appendChild(td);
             let div_1 = document.createElement("div");
             div_1.className = "content";
+
+
+
             let div_2 = document.createElement("div");
             div_2.className = "sb-content__calendar-note-1";
             let div_2_inner = document.createElement("div");
@@ -19,18 +24,24 @@ var a = function () {
             let a = document.createElement("div");
             a.className = "sb-content__calendar-todo-link tooltip";
 
-
-
             if(counter === 3 && counter === 7 && counter === 10 ) {
 
             }
 
             div_1.innerHTML = `${counter}`;
+            if(counter === 32) {
+                div_1.innerHTML = `Sept 1`;
+                counter = 1;
+            }
             td.appendChild(div_1);
-            tableWrapper.appendChild(td);
-
 
             counter++;
+
         }
+
+        tableWrapper.appendChild(tr);
+
     }
 };
+
+a();
